9b8906d153af96deecf8dc5c5f8cf4d1338bda68
nicolas briet <nicolas.briet@outlook.com>
2018-11-05 12:43:13 +0100

    fin etape2

--- scriptJSetape1.js
+++ scriptJSetape1.js
@@ -10,15 +10,14 @@ function initBt(){
 		$('#showliste').replaceWith(mylist)
 		data.lines.line.map((item , i) => {
 			let newli = $( '<li style=\'color:' + item.bgXmlColor + '\'>' + item.name + '</li>' )
-			newli.click(() => clickLigne(item.id))
+			newli.click(() => clickLigne(item))
 			mylist.append(newli)
 		}, mylist)
 	})
 }
 
-function clickLigne(id){
-	console.log(id)
-	let tempUrl = 'https://api.tisseo.fr/v1/stop_points.json?key=a3732a1074e2403ce364ad6e71eb998cb&displayCoordXY=1&lineId='+id
+function clickLigne(item){
+	let tempUrl = 'https://api.tisseo.fr/v1/stop_points.json?key=a3732a1074e2403ce364ad6e71eb998cb&displayCoordXY=1&lineId='+item.id
 	$.ajax({
 		type : "GET",
 		url : tempUrl
@@ -27,10 +26,9 @@ function clickLigne(id){
 		let myStopList = $('<ul/>')
 		myStopList.attr('id' , 'showstop')
 		$('#showstop').replaceWith(myStopList)
+		myStopList.append($('<h1> Arrets de la lignes ' + item.shortName + ' </h1>'))
 		data.physicalStops.physicalStop.map((item , i) => {
-				console.log(item.name)
-				// let newli = $ )
-				myStopList.append($('<li>' + item.name + '</li>'))
+				myStopList.append($('<li>' + item.name +' ( '+ item.x +','+ item.y +' )</li>'))
 			}, myStopList)
 	})
 }
