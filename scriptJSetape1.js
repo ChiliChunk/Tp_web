0cd082d717c05836a708d687765ae4dd6fd9261e
nicolas briet <nicolas.briet@outlook.com>
2018-11-05 12:25:57 +0100

    arret de bus

--- scriptJSetape1.js
+++ scriptJSetape1.js
@@ -9,7 +9,28 @@ function initBt(){
 		mylist.attr('id' , 'showliste')
 		$('#showliste').replaceWith(mylist)
 		data.lines.line.map((item , i) => {
-			mylist.append( '<li style=\'color:' + item.bgXmlColor + '\'>' + item.name + '</li>' )
+			let newli = $( '<li style=\'color:' + item.bgXmlColor + '\'>' + item.name + '</li>' )
+			newli.click(() => clickLigne(item.id))
+			mylist.append(newli)
 		}, mylist)
+	})
+}
+
+function clickLigne(id){
+	console.log(id)
+	let tempUrl = 'https://api.tisseo.fr/v1/stop_points.json?key=a3732a1074e2403ce364ad6e71eb998cb&displayCoordXY=1&lineId='+id
+	$.ajax({
+		type : "GET",
+		url : tempUrl
+	}).done((data) => {
+		console.log(data)
+		let myStopList = $('<ul/>')
+		myStopList.attr('id' , 'showstop')
+		$('#showstop').replaceWith(myStopList)
+		data.physicalStops.physicalStop.map((item , i) => {
+				console.log(item.name)
+				// let newli = $ )
+				myStopList.append($('<li>' + item.name + '</li>'))
+			}, myStopList)
 	})
 }
